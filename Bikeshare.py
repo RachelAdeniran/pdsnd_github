e697f63f91dd410160fb029240918274c0c998de
Adeniran Rachel Ihunanya <ihunanyayekini@gmail.com>
2022-01-08 17:43:50 +0100

    comment adjustment

--- Bikeshare.py
+++ Bikeshare.py
@@ -22,7 +22,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    #get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    #getting user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input('Please input a city: ')
         city = city.lower()
